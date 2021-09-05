764eaf38e315073f4a6576c07aff62ef48699647
Melvin Gan <melvingan2811@gmail.com>
2021-09-05 16:43:35 +0800

    Added new component Cart and updated products

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
 
 //Components
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -13,13 +14,26 @@ const App = () => {
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+  console.log(cart);
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   );
 };
