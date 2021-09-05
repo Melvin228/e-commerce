4ef2b22fd398f04db3dfe4a1a5299d657539a15f
Melvin Gan <melvingan2811@gmail.com>
2021-09-05 16:57:41 +0800

    Added cart item component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Container,Typography,Button,Grid} from '@material-ui/core'
 import useStyles from './styles'
 
+
+//Components
+import CartItem from './CartItem/CartItem'
 const Cart = ({cart}) => {
 
     const classes = useStyles()
@@ -15,7 +18,7 @@ const Cart = ({cart}) => {
             <Grid containing spacing= {3}>
                 {cart.line_items.map((item)=>(
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item}></CartItem>
                     </Grid>
                 ))}
             </Grid>
